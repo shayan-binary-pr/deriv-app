b557bf6b545d8cc59fa308dcadaa8dc9330991cd
shayan-binary-pr <shayan-binary-pr@protonmail.com>
2019-12-19 13:00:06 +0330

    Updates

---
+++ br_graph-issue/js/bot/bot.text_print.c312edd550b18762cc4b.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["text_print"],{
+
+/***/ "./src/scratch/help-content/help-strings/text_print.js":
+/*!*************************************************************!*\
+  !*** ./src/scratch/help-content/help-strings/text_print.js ***!
+  \*************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! deriv-translations */ \"deriv-translations\");\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(deriv_translations__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  text: [Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('This block displays a modal window with a given message.'), Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('Strategy execution is being paused until user clicks \"OK\"')]\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmdzL3RleHRfcHJpbnQuanMuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kZXJpdi1ib3QvLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmdzL3RleHRfcHJpbnQuanM/N2MyMSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBsb2NhbGl6ZSB9IGZyb20gJ2Rlcml2LXRyYW5zbGF0aW9ucyc7XG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgICB0ZXh0OiBbXG4gICAgICAgIGxvY2FsaXplKCdUaGlzIGJsb2NrIGRpc3BsYXlzIGEgbW9kYWwgd2luZG93IHdpdGggYSBnaXZlbiBtZXNzYWdlLicpLFxuICAgICAgICBsb2NhbGl6ZSgnU3RyYXRlZ3kgZXhlY3V0aW9uIGlzIGJlaW5nIHBhdXNlZCB1bnRpbCB1c2VyIGNsaWNrcyBcIk9LXCInKSxcbiAgICBdLFxufTtcbiJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQUE7QUFFQTtBQUNBO0FBREEiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-strings/text_print.js\n");
+
+/***/ })
+
+}]);
---
+++ br_graph-issue/js/bot/bot.text_prompt_ext.fd200ceab8ef766de743.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["text_prompt_ext"],{
+
+/***/ "./src/scratch/help-content/help-strings/text_prompt_ext.js":
+/*!******************************************************************!*\
+  !*** ./src/scratch/help-content/help-strings/text_prompt_ext.js ***!
+  \******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! deriv-translations */ \"deriv-translations\");\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(deriv_translations__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  text: [Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('This block creates a dialog box that uses a customised message to prompt for an input. The input can be either a string of text or a number.'), Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('The input can be assigned to a variable to be used in your strategy.')]\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmdzL3RleHRfcHJvbXB0X2V4dC5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovL2Rlcml2LWJvdC8uL3NyYy9zY3JhdGNoL2hlbHAtY29udGVudC9oZWxwLXN0cmluZ3MvdGV4dF9wcm9tcHRfZXh0LmpzP2QzZmMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgbG9jYWxpemUgfSBmcm9tICdkZXJpdi10cmFuc2xhdGlvbnMnO1xuXG5leHBvcnQgZGVmYXVsdCB7XG4gICAgdGV4dDogW1xuICAgICAgICBsb2NhbGl6ZSgnVGhpcyBibG9jayBjcmVhdGVzIGEgZGlhbG9nIGJveCB0aGF0IHVzZXMgYSBjdXN0b21pc2VkIG1lc3NhZ2UgdG8gcHJvbXB0IGZvciBhbiBpbnB1dC4gVGhlIGlucHV0IGNhbiBiZSBlaXRoZXIgYSBzdHJpbmcgb2YgdGV4dCBvciBhIG51bWJlci4nKSxcbiAgICAgICAgbG9jYWxpemUoJ1RoZSBpbnB1dCBjYW4gYmUgYXNzaWduZWQgdG8gYSB2YXJpYWJsZSB0byBiZSB1c2VkIGluIHlvdXIgc3RyYXRlZ3kuJyksXG4gICAgXSxcbn07XG4iXSwibWFwcGluZ3MiOiJBQUFBO0FBQUE7QUFBQTtBQUFBO0FBRUE7QUFDQTtBQURBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-strings/text_prompt_ext.js\n");
+
+/***/ })
+
+}]);
