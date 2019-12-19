9773751b7cf38ef0fa83b974a25e46020a4d6893
shayan-binary-pr <shayan-binary-pr@protonmail.com>
2019-12-19 12:38:18 +0330

    Updates

---
+++ trader/js/bot/bot.read_ohlc_obj.e8afa308650c009ebfe6.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["read_ohlc_obj"],{
+
+/***/ "./src/scratch/help-content/help-strings/read_ohlc_obj.js":
+/*!****************************************************************!*\
+  !*** ./src/scratch/help-content/help-strings/read_ohlc_obj.js ***!
+  \****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! deriv-translations */ \"deriv-translations\");\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(deriv_translations__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  text: [Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('This block gives you the selected candle value such as open price, close price, high price, low price, and open time. It requires a candle as an input parameter.'), Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('In the example below, the open price is assigned to the variable \"op\".')]\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmdzL3JlYWRfb2hsY19vYmouanMuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kZXJpdi1ib3QvLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmdzL3JlYWRfb2hsY19vYmouanM/N2JkOSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBsb2NhbGl6ZSB9IGZyb20gJ2Rlcml2LXRyYW5zbGF0aW9ucyc7XG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgICB0ZXh0OiBbXG4gICAgICAgIGxvY2FsaXplKCdUaGlzIGJsb2NrIGdpdmVzIHlvdSB0aGUgc2VsZWN0ZWQgY2FuZGxlIHZhbHVlIHN1Y2ggYXMgb3BlbiBwcmljZSwgY2xvc2UgcHJpY2UsIGhpZ2ggcHJpY2UsIGxvdyBwcmljZSwgYW5kIG9wZW4gdGltZS4gSXQgcmVxdWlyZXMgYSBjYW5kbGUgYXMgYW4gaW5wdXQgcGFyYW1ldGVyLicpLFxuICAgICAgICBsb2NhbGl6ZSgnSW4gdGhlIGV4YW1wbGUgYmVsb3csIHRoZSBvcGVuIHByaWNlIGlzIGFzc2lnbmVkIHRvIHRoZSB2YXJpYWJsZSBcIm9wXCIuJyksXG4gICAgXSxcbn07XG4iXSwibWFwcGluZ3MiOiJBQUFBO0FBQUE7QUFBQTtBQUFBO0FBRUE7QUFDQTtBQURBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-strings/read_ohlc_obj.js\n");
+
+/***/ })
+
+}]);
