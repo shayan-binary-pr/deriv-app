21d8a9e7f9c45351d516dcd4b5a605605e2ab2a7
shayan-binary-pr <shayan-binary-pr@protonmail.com>
2019-12-23 06:37:03 +0330

    Updates

---
+++ br_graph-issue/js/bot/bot.sell_price.9855a65d38c9751b3f29.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["sell_price"],{
+
+/***/ "./src/scratch/help-content/help-strings/sell_price.js":
+/*!*************************************************************!*\
+  !*** ./src/scratch/help-content/help-strings/sell_price.js ***!
+  \*************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! deriv-translations */ \"deriv-translations\");\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(deriv_translations__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  text: [Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('This block gives you the potential profit or loss if you decide to sell your contract. It can only be used within the \"Sell conditions\" root block.'), Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('In the example below, the contract will only be sold if the potential profit or loss is more than the stake.')]\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmdzL3NlbGxfcHJpY2UuanMuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kZXJpdi1ib3QvLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmdzL3NlbGxfcHJpY2UuanM/MWMyMCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBsb2NhbGl6ZSB9IGZyb20gJ2Rlcml2LXRyYW5zbGF0aW9ucyc7XG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgICB0ZXh0OiBbXG4gICAgICAgIGxvY2FsaXplKCdUaGlzIGJsb2NrIGdpdmVzIHlvdSB0aGUgcG90ZW50aWFsIHByb2ZpdCBvciBsb3NzIGlmIHlvdSBkZWNpZGUgdG8gc2VsbCB5b3VyIGNvbnRyYWN0LiBJdCBjYW4gb25seSBiZSB1c2VkIHdpdGhpbiB0aGUgXCJTZWxsIGNvbmRpdGlvbnNcIiByb290IGJsb2NrLicpLFxuICAgICAgICBsb2NhbGl6ZSgnSW4gdGhlIGV4YW1wbGUgYmVsb3csIHRoZSBjb250cmFjdCB3aWxsIG9ubHkgYmUgc29sZCBpZiB0aGUgcG90ZW50aWFsIHByb2ZpdCBvciBsb3NzIGlzIG1vcmUgdGhhbiB0aGUgc3Rha2UuJyksXG4gICAgXSxcbn07XG4iXSwibWFwcGluZ3MiOiJBQUFBO0FBQUE7QUFBQTtBQUFBO0FBRUE7QUFDQTtBQURBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-strings/sell_price.js\n");
+
+/***/ })
+
+}]);
---
+++ br_graph-issue/js/bot/bot.totimestamp.b5148305a3713b987bbb.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([["totimestamp"],{
+
+/***/ "./src/scratch/help-content/help-strings/totimestamp.js":
+/*!**************************************************************!*\
+  !*** ./src/scratch/help-content/help-strings/totimestamp.js ***!
+  \**************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! deriv-translations */ \"deriv-translations\");\n/* harmony import */ var deriv_translations__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(deriv_translations__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  text: [Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('This block converts the date and time to the number of seconds since the Unix Epoch (1970-01-01 00:00:00).'), Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('Example:'), Object(deriv_translations__WEBPACK_IMPORTED_MODULE_0__[\"localize\"])('In this example, a Rise contract will be purchased at midnight on 1 August 2019.')]\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmdzL3RvdGltZXN0YW1wLmpzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZGVyaXYtYm90Ly4vc3JjL3NjcmF0Y2gvaGVscC1jb250ZW50L2hlbHAtc3RyaW5ncy90b3RpbWVzdGFtcC5qcz83ZTYzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGxvY2FsaXplIH0gZnJvbSAnZGVyaXYtdHJhbnNsYXRpb25zJztcblxuZXhwb3J0IGRlZmF1bHQge1xuICAgIHRleHQ6IFtcbiAgICAgICAgbG9jYWxpemUoJ1RoaXMgYmxvY2sgY29udmVydHMgdGhlIGRhdGUgYW5kIHRpbWUgdG8gdGhlIG51bWJlciBvZiBzZWNvbmRzIHNpbmNlIHRoZSBVbml4IEVwb2NoICgxOTcwLTAxLTAxIDAwOjAwOjAwKS4nKSxcbiAgICAgICAgbG9jYWxpemUoJ0V4YW1wbGU6JyksXG4gICAgICAgIGxvY2FsaXplKCdJbiB0aGlzIGV4YW1wbGUsIGEgUmlzZSBjb250cmFjdCB3aWxsIGJlIHB1cmNoYXNlZCBhdCBtaWRuaWdodCBvbiAxIEF1Z3VzdCAyMDE5LicpLFxuICAgIF0sXG59O1xuIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUFBO0FBQUE7QUFBQTtBQUVBO0FBQ0E7QUFEQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-strings/totimestamp.js\n");
+
+/***/ })
+
+}]);
